d2516772178b9fb3bbd708d703042e8b8fb1c45e
HEMANTH123SR <applicationcomputer257@gmail.com>
2024-05-29 21:40:19 +0530

    add absoulte 3d like png image for background

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-"use client";
-import { ArrowRight } from "lucide-react";
-import { GetStartedBtn } from "@/components/small-client-component/getStartedBtn"
+import { GetStartedBtn } from "@/components/small-client-component/getStartedBtn";
 import DotPattern from "@/components/magicui/dot-pattern";
 export default function Home() {
   return (
     <main className="min-h-screen h-full flex flex-col w-full">
       {/* hero section */}
-      <div className="relative overflow-hidden">
-        <section className="flex flex-col items-center rounded-2xl h-[90vh] bg-[#F6F6F8] mx-8 my-4 border p-4">
+      <div className="relative overflow-hidden my-4">
+        <section className="flex flex-col items-center rounded-2xl h-[90vh] bg-[#F6F6F8] mx-8  border p-4">
           <div className="absolute flex h-full w-full  overflow-hidden  gap-8 opacity-55  ">
             {Array.from({ length: 3 }).map((_, i) => (
               <DotPattern
@@ -30,7 +28,6 @@ export default function Home() {
             </h1>
             <h1 className="px-24 text-7xl text-black ">Web Portfolio </h1>
           </div>
-          {/* Forget the Hustle of No-Code Tools - Create a Stunning Portfolio in 2 Clicks */}
           <div className="flex flex-col mt-6 z-10 justify-center items-center">
             <p>Forget the Hustle of No-Code Tools and Programming</p>
             <p> Create a Stunning Portfolio in 2 Clicks</p>
@@ -38,6 +35,10 @@ export default function Home() {
           <div className="z-10">
             <GetStartedBtn />
           </div>
+          <img src="/cursor-4.png" className="absolute z-20 right-10 bottom-10 h-48 w-auto mr-12 mb-12 -rotate-12" />
+          <img src="/linkedin.png" className="absolute z-20 left-10 bottom-10 h-48 w-auto ml-6 mb-12 rotate-12" />
+          <img src="/website-1.png" className="absolute z-0 right-1/4 -top-24 h-72 w-auto ml-24 mb-12 rotate-12" />
+
         </section>
       </div>
       <div className="flex h-96 w-96"></div>
