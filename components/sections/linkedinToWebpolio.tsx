76e90374f28f9bba49c7bda33861e97fe54ae191
HEMANTH123SR <applicationcomputer257@gmail.com>
2024-06-01 10:35:40 +0530

    ui chnage on linkedinToWebpolio section

--- components/sections/linkedinToWebpolio.tsx
+++ components/sections/linkedinToWebpolio.tsx
@@ -2,7 +2,7 @@ import { ArrowRight } from 'lucide-react';
 
 export const LinkedinToWebpolio = () => {
     return (
-        <div className=" flex flex-col justify-between items-center mx-8">
+        <div className=" flex flex-col justify-between items-center mx-8 bg-[#F7F9FB] rounded-xl border py-16">
             <h2 className="text-5xl font-medium">
                 Build Your Portfolio In 2 Minutes
             </h2>
@@ -11,26 +11,29 @@ export const LinkedinToWebpolio = () => {
                 web portfolio
             </p>
             <div className="flex space-x-12 justify-center items-center my-16">
+
                 <div className="flex flex-col justify-center items-center space-y-2">
                     <img src="/linkedin-logo.webp" className="h-12 w-12 rounded-xl" />
-                    <div className='flex flex-col border rounded-t-lg'>
-                        <div className='bg-black  h-4 rounded-t-lg px-4 flex items-center space-x-1.5'>
-                            <div className='h-1.5 w-1.5 bg-white rounded-full'></div>
-                            <div className='h-1.5 w-1.5 bg-white rounded-full'></div>
-                            <div className='h-1.5 w-1.5 bg-white rounded-full'></div>
+                    <div className='flex flex-col border border-t-0'>
+                        <div className='bg-white border  h-4  px-4 flex items-center space-x-1.5'>
+                            <div className='h-1.5 w-1.5 bg-[#C7C6C6] rounded-full'></div>
+                            <div className='h-1.5 w-1.5 bg-[#C7C6C6] rounded-full'></div>
+                            <div className='h-1.5 w-1.5 bg-[#C7C6C6] rounded-full'></div>
                         </div>
                         <img src="/linkedin-to-webpolio.png" className="w-96 h-auto" />
                     </div>
                     <span className="text-sm text-slate-600">From Linkedin</span>
                 </div>
+
                 <ArrowRight className='h-10 w-10' />
+
                 <div className="flex flex-col justify-center items-center space-y-2">
                     <img src="/logo2.png" className="h-12 w-12 rounded-xl" />
-                    <div className='flex flex-col border rounded-t-lg'>
-                        <div className='bg-black  h-4 rounded-t-lg px-4 flex items-center space-x-1.5'>
-                            <div className='h-1.5 w-1.5 bg-white rounded-full'></div>
-                            <div className='h-1.5 w-1.5 bg-white rounded-full'></div>
-                            <div className='h-1.5 w-1.5 bg-white rounded-full'></div>
+                    <div className='flex flex-col border border-t-0 '>
+                        <div className='bg-white border  h-4  px-4 flex items-center space-x-1.5'>
+                            <div className='h-1.5 w-1.5 bg-[#C7C6C6] rounded-full'></div>
+                            <div className='h-1.5 w-1.5 bg-[#C7C6C6] rounded-full'></div>
+                            <div className='h-1.5 w-1.5 bg-[#C7C6C6] rounded-full'></div>
                         </div>
                         <img src="/webpolio-to-linkedin.png" className="w-96 h-auto" />
                     </div>
