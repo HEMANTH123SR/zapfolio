6c80c0b9bff39d0984f057efd71deb006dad85ce
HEMANTH123SR <applicationcomputer257@gmail.com>
2024-06-03 15:17:40 +0530

    hero section responsive for md lg xl and bigger width screens

--- components/sections/heroSection.tsx
+++ components/sections/heroSection.tsx
@@ -2,9 +2,9 @@ import { GetStartedBtn } from "@/components/small-client-component/getStartedBtn
 import DotPattern from "@/components/magicui/dot-pattern";
 export const HeroSection = () => {
     return (
-        <div className="relative overflow-hidden w-[95%] max-w-[1400px]">
-            <section className="flex flex-col items-center rounded-2xl  bg-[#F6F6F8]   border p-4">
-                <div className="absolute flex h-full w-full  overflow-hidden  gap-8 opacity-55">
+        <div className="relative w-[95%] max-w-[1400px] overflow-hidden">
+            <section className="flex flex-col items-center rounded-2xl border bg-[#F6F6F8] p-4">
+                <div className="absolute flex h-full w-full gap-8 overflow-hidden opacity-55">
                     {Array.from({ length: 3 }).map((_, i) => (
                         <DotPattern
                             key={i}
@@ -17,16 +17,18 @@ export const HeroSection = () => {
                     ))}
                 </div>
                 <div className="h-20"></div>
-                <span className="border-[#FF560E] border bg-white text-black rounded-full px-6 py-1.5 flex justify-center items-center space-x-2 z-20 text-sm cursor-pointer">
+                <span className="z-20 flex cursor-pointer items-center justify-center space-x-2 rounded-full border-2 border-[#FF560E] bg-white px-4 py-1.5 text-sm text-black">
                     LinkedIn Meets Web Portfolio
                 </span>
-                <div className="flex flex-col justify-center items-center  mt-6 capitalize font-serif z-10">
-                    <h1 className="px-24 text-7xl text-black ">
+                <div className="z-30 mt-6 flex flex-col items-center justify-center font-serif capitalize">
+                    <h1 className="text-3xl text-black md:text-5xl lg:text-6xl xl:text-7xl">
                         Convert LinkedIn Profile into{" "}
                     </h1>
-                    <h1 className="px-24 text-7xl text-black ">Web Portfolio </h1>
+                    <h1 className="text-3xl text-black md:text-5xl lg:text-6xl xl:text-7xl">
+                        Web Portfolio{" "}
+                    </h1>
                 </div>
-                <div className="flex flex-col mt-6 z-10 justify-center items-center">
+                <div className="z-30 mt-6 flex flex-col items-center justify-center">
                     <p>Forget the Hustle of No-Code Tools and Programming</p>
                     <p> Create a Stunning Portfolio in 2 Clicks</p>
                 </div>
@@ -35,15 +37,15 @@ export const HeroSection = () => {
                 </div>
                 <img
                     src="/cursor-4.png"
-                    className="absolute z-20 right-10 bottom-10 h-48 w-auto mr-12 mb-12 -rotate-12"
+                    className="absolute bottom-2 right-2 lg:bottom-10 lg:right-10 z-20 mb-4 mr-4 lg:mb-12 lg:mr-12 h-48 w-auto -rotate-12"
                 />
                 <img
                     src="/linkedin.png"
-                    className="absolute z-20 left-10 bottom-10 h-48 w-auto ml-6 mb-12 rotate-12"
+                    className="absolute bottom-2 left-2 lg:bottom-10 lg:left-10 z-20 mb-4 ml-3 lg:mb-12 lg:ml-6 h-48 w-auto rotate-12"
                 />
                 <img
                     src="/website-1.png"
-                    className="absolute z-0 right-1/4 -top-24 h-72 w-auto ml-24 mb-12 rotate-12"
+                    className="absolute -top-24 right-1/4 z-0 mb-12 ml-24 h-72 w-auto rotate-12"
                 />
             </section>
         </div>
