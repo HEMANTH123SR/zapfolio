529e0912ce305cadfe4ebccdc306d17dd437b32d
HEMANTH123SR <applicationcomputer257@gmail.com>
2024-06-18 19:49:32 +0530

    add responsiveness  on linkedin-url page

--- app/linkedin-url/page.tsx
+++ app/linkedin-url/page.tsx
@@ -4,19 +4,32 @@ const LinkedinUrlAndJobTitle = async () => {
     const user = await currentUser();
     console.log(user);
     return (
-        <main className="w-full px-8">
-            <div className="flex w-full justify-center rounded-lg border bg-[#FF560E] p-14">
-                <div className="flex pb-14 w-[600px] flex-col items-center border shadow-2xl   rounded-lg bg-white">
-                    <img src="/zapfolio-logo/2.png" className="w-1/4 py-4" />
-                    <div className="w-full border"></div>
+        <main className="mb-12 mt-2 flex w-full justify-center items-center h-[70vh] sm:h-auto sm:px-8 border-t-2">
+            <div className="flex w-full max-w-[1400px] justify-center   sm:rounded-xl sm:border sm:bg-[#FF560E] sm:pb-14 sm:pt-14">
+                <div className="flex w-full flex-col items-center rounded-lg bg-white pb-14 sm:w-11/12 sm:border sm:shadow-2xl md:w-[600px]">
+                    <img
+                        src="/zapfolio-logo/2.png"
+                        className="hidden w-1/4 py-4 sm:block"
+                    />
+                    <div className="w-full border hidden sm:block"></div>
                     <h1
-                        className={`mt-14 mb-7 text-4xl  text-[#FF560E] font-semibold`}
+                        className={`mb-7 mt-14 text-4xl mx-4  font-semibold text-[#FF560E] sm:text-4xl`}
                     >
-                        Enter Your Linkedin Url
+                        Enter Your Linkedin Profile Url
                     </h1>
-                    <input className="w-8/12 bg-[#F1F4F9] text-black px-5 p-2 border rounded-md" placeholder="https://www.linkedin.com/in/yourprofile" />
-                    <Link className="underline text-sm my-6 text-[#666C7E]" href={"/find-linkedin-url"} >How do I find my LinkedIn URL?</Link>
-                    <button className="bg-[#FF560E] rounded-md text-white text-lg px-10 font-semibold py-1.5">Continue</button>
+                    <input
+                        className="w-10/12 sm:w-8/12 rounded-md border bg-[#F1F4F9] p-2 px-5 text-black"
+                        placeholder="https://www.linkedin.com/in/yourprofile"
+                    />
+                    <Link
+                        className="my-6 text-sm text-[#666C7E] underline"
+                        href={"/find-linkedin-url"}
+                    >
+                        How do I find my LinkedIn URL?
+                    </Link>
+                    <button className="rounded-md bg-[#FF560E] w-10/12 sm:w-8/12  py-1.5 text-lg font-semibold text-white">
+                        Continue
+                    </button>
                 </div>
             </div>
         </main>
