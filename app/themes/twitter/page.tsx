9329acf601d0269c57b201619d06dd7e9530e16c
HEMANTH123SR <applicationcomputer257@gmail.com>
2024-06-27 11:07:21 +0530

    add twitter theme

---
+++ app/themes/twitter/page.tsx
@@ -0,0 +1,8 @@
+export default function RootLayout() {
+    return (
+        <div className="flex h-full w-full">
+            <div className="flex h-screen min-w-[300px] max-w-[300px]  border bg-[#FAFAFA]  border-t-4 border-t-[rgb(255,86,14)]"></div>
+            <iframe src="https://twitter.zapfolio.in/hello" className="h-screen w-full border-r border-t-4 border-[#FF560E]" />
+        </div>
+    );
+}
