2459bcd6248121685f41314cefa8eea8b0f67265
hemanthsr <applicationcomputer257@gmail.com>
2024-08-21 14:53:11 +0530

    minor code module type  changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,15 @@ export default function Home() {
     <div className="flex h-full w-full flex-col items-center justify-center overflow-x-hidden bg-[#F6F6F8] sm:bg-white">
       <div className="mt-2"></div>
       <HeroSection />
-      <div className="my-3.5 sm:my-6 md:my-8 lg:my-10 xl:my-12"></div>
+      <div className="my-3.5 sm:my-6 md:my-8 lg:my-4"></div>
       <LinkedinToWebpolio />
-      <div className="my-3.5 sm:my-6 md:my-8 lg:my-10 xl:my-12"></div>
+      <div className="my-3.5 sm:my-6 md:my-8 lg:my-4"></div>
       <ExampleSection />
-      <div className="my-3.5 sm:my-6 md:my-8 lg:my-10 xl:my-12"></div>
+      <div className="my-3.5 sm:my-6 md:my-8 lg:my-4"></div>
       <TestimonialsSection />
-      <div className="my-3.5 sm:my-6 md:my-8 lg:my-10 xl:my-12"></div>
+      <div className="my-3.5 sm:my-6 md:my-8 lg:my-4"></div>
       <FaqSection />
-      <div className="my-3.5 sm:my-6 md:my-8 lg:my-10 xl:my-12"></div>
+      <div className="my-3.5 sm:my-6 md:my-8 lg:my-4"></div>
     </div>
   );
 }
