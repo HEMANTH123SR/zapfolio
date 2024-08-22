d19e7a564cebaa7a111a0ac8b7ad979837477609
hemanthsr <applicationcomputer257@gmail.com>
2024-08-22 11:05:49 +0530

    fixed theme/slate trying to use child when there is no child  bug fixed

--- app/themes/slate/page.tsx
+++ app/themes/slate/page.tsx
@@ -3,53 +3,51 @@ import { useEffect, useState } from "react";
 import { useUser } from "@clerk/clerk-react";
 import { ScreenComponent } from "@/components/component/themeScreenComponent";
 
+const Page = () => {
+  const { user, isLoaded } = useUser();
+  const [userExistes, setUserExistes] = useState<boolean>(false);
+  const [userDataLoaded, setUserDataLoaded] = useState<boolean>(false);
+  const [isDesktop, setIsDesktop] = useState(true);
+  const toggleDevice = () => {
+    setIsDesktop(!isDesktop);
+  };
 
-const Page = ({ children }: { children: React.ReactNode }) => {
-    const { user, isLoaded } = useUser();
-    const [userExistes, setUserExistes] = useState<boolean>(false);
-    const [userDataLoaded, setUserDataLoaded] = useState<boolean>(false);
-    const [isDesktop, setIsDesktop] = useState(true);
-    const toggleDevice = () => {
-        setIsDesktop(!isDesktop);
-    };
-
-    useEffect(() => {
-        if (isLoaded) {
-            (async () => {
-                const userResponseDataJson = await fetch(
-                    `/api/get-user-data?userId=${user?.id}`,
-                );
-                const userResponseData = await userResponseDataJson.json();
-                if (userResponseData.success) {
-                    console.log("user data exists");
-                    setUserExistes(true);
-                }
-                setUserDataLoaded(true);
-            })();
-        }
-    }, [isLoaded, user?.id]);
-    if (isLoaded && user) {
-        return (
-            <div className="relative h-screen w-full">
-                <ScreenComponent
-                    isDesktop={isDesktop}
-                    link={`https://slate.zapfolio.in/${user.id}`}
-                    toggleDevice={toggleDevice}
-                />
-                <iframe
-                    src={`https://slate.zapfolio.in/${user.id}`}
-                    className={`${!isDesktop && "border"} hidden h-screen md:block`}
-                    style={{
-                        width: isDesktop ? "100%" : "375px",
-                        margin: "auto",
-                    }}
-                />
-            </div>
+  useEffect(() => {
+    if (isLoaded) {
+      (async () => {
+        const userResponseDataJson = await fetch(
+          `/api/get-user-data?userId=${user?.id}`,
         );
+        const userResponseData = await userResponseDataJson.json();
+        if (userResponseData.success) {
+          console.log("user data exists");
+          setUserExistes(true);
+        }
+        setUserDataLoaded(true);
+      })();
     }
+  }, [isLoaded, user?.id]);
+  if (isLoaded && user) {
+    return (
+      <div className="relative h-screen w-full">
+        <ScreenComponent
+          isDesktop={isDesktop}
+          link={`https://slate.zapfolio.in/${user.id}`}
+          toggleDevice={toggleDevice}
+        />
+        <iframe
+          src={`https://slate.zapfolio.in/${user.id}`}
+          className={`${!isDesktop && "border"} hidden h-screen md:block`}
+          style={{
+            width: isDesktop ? "100%" : "375px",
+            margin: "auto",
+          }}
+        />
+      </div>
+    );
+  }
 
-    return <div>loading</div>
-
+  return <div>loading</div>;
 };
 
 export default Page;
